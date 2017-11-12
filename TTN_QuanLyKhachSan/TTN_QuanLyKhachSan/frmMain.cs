01b6d76c0092ddc285fed4838a80d8187a70dd0a
hitlun <32055397+hitlun@users.noreply.github.com>
2017-11-12 23:50:44 +0700

    Project First

---
+++ TTN_QuanLyKhachSan/TTN_QuanLyKhachSan/frmMain.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TTN_QuanLyKhachSan
+{
+    public partial class frmMain : Form
+    {
+        public static string _taikhoan = "";
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnNguoiDung_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void thoátToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void đăngNhậpLạiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void frmMain_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void btnVatDung_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThuePhong_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThanhToan_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDichVu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPhong_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKhachHang_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSuDungDV_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hướngDẫnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
